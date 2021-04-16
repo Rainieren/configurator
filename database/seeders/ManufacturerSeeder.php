25ee55b9a4e6f772eb809c13735fbebcbeb5ef52
Rainier <rainier.laan@home.nl>
2021-04-16 11:25:38 +0200

    Added the seeders

--- database/seeders/ManufacturerSeeder.php
+++ database/seeders/ManufacturerSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
+use Faker\Factory;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ManufacturerSeeder extends Seeder
 {
@@ -13,6 +16,24 @@ class ManufacturerSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Factory::create();
+        // Make 100 new Products
+        foreach(range(1, 100) as $index) {
+
+
+            DB::table('manufacturers')->insert([
+
+                'name' => $faker->word,
+                'address' => $faker->streetAddress,
+                'zip_code' => $faker->postcode,
+                'city' => $faker->city,
+                'country_id' => $faker->numberBetween($min = 1, $max = 249),
+
+
+                'created_at' => Carbon::now(),
+
+                'updated_at' => Carbon::now(),
+            ]);
+        }
     }
 }
