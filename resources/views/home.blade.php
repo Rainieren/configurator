52557e1cc1165618b38644232810268026dae0fb
Rainier <rainier.laan@home.nl>
2021-04-21 13:49:14 +0200

    Changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,59 +14,34 @@
 
         <div class="configurator bg-white xl:w-8/12 w-full min-h-screen p-20 space-y-15">
             <h1 class="text-5xl font-serif mb-20">Product configureren</h1>
-            <div class="container mx-auto bg-white h-50 w-full">
-                @for($i = 0; $i < 2; $i++)
-                <section class="step">
-                    <p class="step-title text-3xl font-serif my-8">Stap <?= $i + 1 ?></p>
-                    <div class="flex space-x-4">
-                        @for($x = 0; $x < 3; $x++)
-                            <div class="bg-white border-2 <?= $x == 2 ? 'border-indigo-600' : 'border-gray-200' ?> hover:border-indigo-500 hover w-1/4 my-5 rounded-lg cursor-pointer hover:shadow-lg transition-all">
-                                <div class="border-b border-gray-300 p-5 flex">
-                                    <div class="w-1/2 font-bold flex items-center">
-                                        Product <?= $x + 1 ?>
-                                        <svg class="text-gray-500 h-6 w-6 mx-3" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 32 32" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                        </svg>
-                                    </div>
-                                    <div class="w-1/2 text-right text-sm">
-                                        + € 5,00
-                                    </div>
-                                </div>
-                                <div class="w-full flex justify-center items-center h-40">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                                    </svg>
-                                </div>
-                                <div class="px-10 py-5">
-                                    <p class="text-xs text-gray-400 font-bold">Kleine omschrijving van het product of dienst. Mag niet langer zijn dan 64 tekens...</p>
-                                </div>
-                            </div>
-                        @endfor
-                            <div class="bg-gray-50 border-2 border-gray-200 hover w-1/4 my-5 rounded-lg cursor-not-allowed transition-all">
-                                <div class="border-b border-gray-300 p-5 flex">
-                                    <div class="w-1/2 font-bold">
-                                        Disabled product
-                                    </div>
-                                    <div class="w-1/2 text-right text-sm">
-                                        + € 5,00
-                                    </div>
-                                </div>
-                                <div class="w-full flex justify-center items-center h-40">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                                    </svg>
-                                </div>
-                                <div class="px-10 py-5">
-                                    <p class="text-xs text-gray-400 font-bold">Kleine omschrijving van het product of dienst.</p>
-                                </div>
-                            </div>
-                    </div>
-
-                </section>
-                @endfor
-                <section class="step">
-                    <p class="step-title text-3xl font-serif my-8">Stap</p>
-                    <div class="flex space-x-4">
+            <div class="flex">
+                <div class="w-1/3">
+                    <p class="text-2xl font-bold my-5">Manufacturers: </p>
+                    @foreach($manufacturers as $manufacturer)
+                        <p><span class="text-blue-600 font-bold"></span>Manufacturer: <?= $manufacturer->name ?></p>
+                        <p class="ml-5">Address: <?= $manufacturer->address ?> </p>
+                        <p class="ml-5">Zip code: <?= $manufacturer->zip_code ?> </p>
+                        <p class="ml-5">City: <?= $manufacturer->city ?> </p>
+                        <p class="ml-5">Country: <?= $manufacturer->country['name'] ?> </p>
+                        <p class="ml-5">Products: </p>
+                        @foreach($manufacturer->products as $m_product)
+                            <p class="ml-10">Product name: <?= $m_product->name ?> </p>
+                        @endforeach
+                    @endforeach
+                </div>
+                <div class="w-1/3">
+                    <p class="text-2xl font-bold my-5">Products: </p>
+                    @foreach($products as $product)
+                        <p><span class="text-blue-600 font-bold">Product name:</span> <?= $product->name ?></p>
+                        <p class="ml-5"><span class="text-green-600 font-bold">Manufacturer:</span> <?= $product->manufacturer['name'] ?></p>
+                        <p class="ml-5 text-yellow-600 font-bold">Sub products:</p>
+                        @foreach($product->products as $subProduct)
+                            <p class="ml-10"><span class="text-purple-600 font-bold">Product name: </span><?= $subProduct['name'] ?></p>
+                            <p class="ml-16"><span class="text-pink-600 font-bold">Manufacturer name:</span> <?= $subProduct->manufacturer['name'] ?></p>
+                        @endforeach
+                    @endforeach
+                </div>
+            </div>
 
                     </div>
                 </section>
