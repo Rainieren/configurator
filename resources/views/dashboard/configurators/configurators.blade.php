b2495f879a96c1b1acf60ff0fe16d32d2612202c
Rainier <rainier.laan@home.nl>
2021-06-22 21:54:45 +0200

    Chnages

--- resources/views/dashboard/configurators/configurators.blade.php
+++ resources/views/dashboard/configurators/configurators.blade.php
@@ -7,7 +7,7 @@
                 <p class="text-3xl font-bold text-gray-900">Configurators</p>
             </div>
             <div class="w-1/2 text-right">
-                <a href="{{ route('createConfigurator') }}" type="button" class="inline-flex justify-center rounded-md border border-indigo-300 shadow-sm p-2 lg:px-4 lg:py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 hover:no-underline focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+                <a href="{{ route('createConfigurator') }}" type="button" class="appearance-none inline-flex justify-center rounded-md border border-indigo-300 shadow-sm p-2 lg:px-4 lg:py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 hover:no-underline focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
                     <span class="hidden lg:block">New configurator</span>
                     <span class="block lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
